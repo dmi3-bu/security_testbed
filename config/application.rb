ccb71ededdfd9bd308b8e3ffa01ba796fe014622
d.bukholtsev <d.bukholtsev@fun-box.ru>
2022-06-15 00:03:45 +1000

    Защита от clickjacking

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module SecurityTestbed
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_dispatch.default_headers['X-Frame-Options'] = 'DENY'
   end
 end
